ea90937d27618cd307cf8358418a86a03d9296e6
vjhansi2021 <vjhansi2021@gmail.com>
2024-12-31 11:59:20 +0530

    Add files via upload

---
+++ dotnet/iandj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program{
+    public static void Main(string[] args){
+        for(int i=0;i<=10;i++){
+            for(int j=0;j<=10;j++){
+                Console.Write(i);
+            }
+        }
+        Console.WriteLine();
+
+    }
+}
---
+++ dotnet/program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program{
+    private static bool j;
+
+    static void Main(String[] args){
+    
+        for(int i=0;i<=50;i++){
+            Console.Write(i);
+        if(i==50){
+        Console.WriteLine("\n");
+        for(int j=50;j>0;--j){
+        Console.Write(j);
+        }
+        Console.Write("\n");
+
+    }
+    }
+}
+}
